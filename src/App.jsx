bca563d69f6950b021db5805e879e198dd2e77b7
weizzx <bobruskadominik@gmail.com>
2024-10-03 03:41:51 +0200

    Fixed progress bar.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Update (){
+  const filled = document.querySelector('.filled');
+  filled.style.width = `${((window.scrollY) / (document.body.scrollHeight - window.innerHeight) * 100)}%`
+  requestAnimationFrame(Update);
+}
 
 function App() {
   const [count, setCount] = useState(0)
+  Update();
   return (
     <>
       <div className="progress-bar"><div className="filled"></div></div>
