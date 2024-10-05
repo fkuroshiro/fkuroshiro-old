89ee014790a7ec9127b123d34db1da473582992f
weizzx <bobruskadominik@gmail.com>
2024-10-05 16:44:44 +0200

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <>
       <div id='home' className='header'>
         <div className="inner">
-          <div><b>Dominik Bobruška</b></div>
+          <div><h1>Dominik Bobruška</h1></div>
           <div><code>Web developer.</code></div>
         </div>
       </div>
@@ -23,11 +23,11 @@ function App() {
         <div className="inner">
           <div className="grid">
             <div className="i">
-              <h1><code>01.</code> About me</h1>
+              <h2><code>01.</code> About me</h2>
               <p>Wassup! My name is Dominik. I'm <code>18</code> years old student. I'm studying high school SOŠGAI Weilova aimed to Informatic Technolgy, currently being in third grade. And my hobby is making websites.</p>
             </div>
             <div className="i">
-              <h1><code>02.</code> Skills</h1>
+              <h2><code>02.</code> Skills</h2>
               <div className='html5'>HTML5<div className="bar"><div className="filled"><code>{HTML5}%</code></div></div></div>
 
               <div className='css3'>CSS3<div className="bar"><div className="filled"><code>{CSS3}%</code></div></div></div>
@@ -42,8 +42,9 @@ function App() {
 
       <section id='projects'>
         <div className="inner">
-          <h1><code>03.</code> Project$</h1>
+          <h2><code>03.</code> Project$</h2>
           <div className="projects-grid">
+            <div className="card">Coming soon...</div>
             <div className="card">Coming soon...</div>
           </div>
         </div>
@@ -52,7 +53,7 @@ function App() {
       <section id='contact'>
         <div className="inner">
           <div className="create-web">
-            <img src={Pencil_Icon} alt="logo" width="200px"/>
+            <img src={Pencil_Icon} alt="logo" width="150px"/>
             <div>
               <h2>Do you want website?</h2>
               <p>Do you want to create a website?</p>
@@ -70,7 +71,7 @@ function App() {
           <div className='body'>
             <div className='creator'>Website created by:<b><a href="">Dominik Bobruška</a></b></div>
             <div className="f-logo">
-              <img src={Logo} alt="logo" width="40px"/>
+              <img src={Logo} alt="logo" width="5%"/>
               <div>weizzx</div>
             </div>
           </div>
